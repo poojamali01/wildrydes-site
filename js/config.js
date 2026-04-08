b93292f596a9c01074a06b4e92ee37c849b42e4d
Pooja Mali <poojatanajimali@gmail.com>
2026-04-08 20:51:13 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-north-1_2ghqwDzvN', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5uf6orph000e6rqe35s41g4b4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-north-1_rshA4N5JJ', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2a3rajfbl6v97lnpq55ck4oqgo', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
