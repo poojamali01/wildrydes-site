c362fab7d420f2987f28f83811528f5cea15ccf7
Pooja Mali <poojatanajimali@gmail.com>
2026-04-08 19:26:28 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-north-1_BorSqx1zB', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7t463jebp63p2913m6jla0pc08', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
